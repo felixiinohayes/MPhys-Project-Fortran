90af78c9587e799a48e3bd1725ba20c36e8475ca
jadgf <felixhayes1@gmail.com>
2024-02-27 18:37:25 +0000

    Added recursive function to find BTP

--- find_BTP.f90
+++ find_BTP.f90
@@ -2,31 +2,27 @@ module parameters
     Implicit None
 !--------to be modified by the user
     character(len=80):: prefix="BiTeI"
-    real*8,parameter::ef= 4.18903772,kxmax=0.0d0,kymax=0.000001,kzmax=0.005,a=0.791
-    integer,parameter::xmeshres=15,ymeshres=15,zmeshres=15,ares=7,nkxpoints=(2*xmeshres+1),nkypoints=(2*ymeshres+1),nkzpoints=(2*zmeshres+1),napoints=(2*ares+1),nbmin=12,nbmax=13,nkp3=nkxpoints*nkypoints*nkzpoints
-    integer nb
-    INTEGER IERR,MYID,NUMPROCS
+    real*8,parameter::ef= 4.18903772,a=0.791
+    integer,parameter::meshres=3,nkp=(2*meshres+1),nkp3=nkp*nkp*nkp
+    integer nb,nr
     
 end module parameters
 
 Program Projected_band_structure
     use parameters
     Implicit None
-    INCLUDE 'mpif.h'
 !------------------------------------------------------
     real*8 dx,dy,dz,da
     character(len=80) top_file,triv_file,nnkp,line
-    integer*4 i,j,k,nr,i1,i2,j1,j2,lwork,info,ikx,iky,ikz,ia,ik,count,kpool,kpmin,kpmax,ecounts,ikp,ir
-    real*8,parameter::third=1d0/3d0, two = 2.0d0, sqrt2 = sqrt(two)
-    real*8 phase,pi2,x1,y1,x2,y2,minbandgap
-    real*8 avec(3,3),bvec(3,3),kpoint(3,nkp3),rvec_data(3)
+    integer*4 i,j,k,i1,i2,j1,j2,lwork,info,ikx,iky,ikz,ia,ik,count,kpool,kpmin,kpmax,ecounts,ikp,ir
+    real*8,parameter::third=1d0/3d0, two = 2.0d0, sqrt2 = sqrt(two), kmax=0.05d0
+    real*8 phase,pi2,x1,y1,x2,y2,minbandgap,bandgap
+    real*8 avec(3,3),bvec(3,3),kpoint(3,nkp3),rvec_data(3),minkpoint(3),kmiddle_initial(3)
     real*8,allocatable:: rvec(:,:),rwork(:)
     real*8, allocatable:: k_ene(:),k_ene_data(:,:),sam(:,:),oam(:,:),kmesh(:,:),energy(:,:),ene(:,:)
     integer*4,allocatable:: ndeg(:)
     complex*16,allocatable:: Hk(:,:),top_Hr(:,:,:),triv_Hr(:,:,:),work(:)
 !------------------------------------------------------
-    call init_mpi
-
     write(top_file,'(a,a)')trim(adjustl(prefix)),"_hr_topological.dat"
     write(triv_file,'(a,a)')trim(adjustl(prefix)),"_hr_trivial.dat"
     write(nnkp,'(a,a)')      trim(adjustl(prefix)),".nnkp"
@@ -46,9 +42,6 @@ Program Projected_band_structure
 !------read H(R)
     open(99,file=trim(adjustl(top_file)))
     open(97,file=trim(adjustl(triv_file)))
-    if(myid.eq.0) then
-        open(100,file='btp_symmetry_2fold.dx')
-    endif
     read(99,*)
     read(99,*)nb,nr
     allocate(rvec(3,nr),Hk(nb,nb),top_Hr(nb,nb,nr),triv_Hr(nb,nb,nr),ndeg(nr))
@@ -69,86 +62,70 @@ Program Projected_band_structure
        rvec(:,k) = rvec_data(1)*avec(:,1) + rvec_data(2)*avec(:,2) + rvec_data(3)*avec(:,3)
     enddo
 
-    lwork=max(1,2*nb-1)
-    allocate(work(max(1,lwork)),rwork(max(1,3*nb-2)))
+	kmiddle_initial = (/0.01704,0.04683,0.474759/)
+	lwork=max(1,2*nb-1)
+	allocate(work(max(1,lwork)),rwork(max(1,3*nb-2)))
+	allocate(k_ene(nb))
 
-    dx=kxmax/xmeshres
-	dy=kymax/ymeshres
-    dz=kzmax/zmeshres
-
-!----- Create header of dx files
-
-    if(myid.eq.0) then
-        write(100, '(a,3(1x,i8))') 'object 1 class gridpositions counts',nkxpoints,nkypoints,nkzpoints
-        write(100, '(a,3(1x,f12.6))') 'origin',0d0,0d0,-kzmax+0.5d0*bvec(3,3)
-        write(100, '(a,3(1x,f12.6))') 'delta',dx,0d0,0d0
-        write(100, '(a,3(1x,f12.6))') 'delta',0d0,dy,0d0
-        write(100, '(a,3(1x,f12.6))') 'delta',0d0,0d0,dz
-        write(100, '(a,3(1x,i8))') 'object 2 class gridconnections counts',nkxpoints,nkypoints,nkzpoints
-    endif
-    
-  !----- Create a uniform k-mesh
-	ik=0
-    do ikx=-xmeshres,xmeshres
-      do iky=-ymeshres,ymeshres
-        do ikz=-zmeshres,zmeshres
-          ik=ik+1
-          kpoint(1,ik)=ikx*dx + 0.01702d0
-          kpoint(2,ik)=iky*dy + 0.046831d0
-          kpoint(3,ik)=ikz*dz + 0.4747d0
-        enddo
-      enddo
-    enddo
-
-    kpool=nkp3/numprocs
-    if (mod(nkp3,numprocs).ne.0) kpool=kpool+1
-
-    kpmin=1+myid*kpool
-    kpmax=(myid+1)*kpool
-
-    ecounts=kpool*2 ! to account for bands 12 and 13
+	call find_minbandgap(kmax,kmiddle_initial,triv_Hr,top_Hr,rvec,ndeg)
 
 
-!----- Perform fourier transform
-    allocate(sam(3,nbmin:nbmax), oam(3,nbmin:nbmax), ene(2,kpool),k_ene(nb),energy(2,nkp3))
+contains
 
-    count=3
-    if(myid.eq.0) then
-        write(100, '(a,i8,a,i8,a,i10,a)') 'object',count,' class array type float rank 1 shape',2,&
-                                     ' item', nkp3, ' data follows'
-    endif
-    count=count+1
+	RECURSIVE SUBROUTINE find_minbandgap(kmax,kmiddle, triv_Hr, top_Hr, rvec,ndeg)
+		use parameters
+		Implicit None
+		real*8 kmiddle(3), kpoint(3,nkp3), rvec(3,nr)
+		real*8 kmax, dk, bandgap, minbandgap, phase
+		integer*4 ndeg(nr),ik,ir,ikx,iky,ikz
+		complex*16 top_Hr(nb,nb,nr),triv_Hr(nb,nb,nr)
+	  !----- Create a uniform k-mesh
+		if (kmax < 0.00001) then
+			print*, kpoint
+			return
+		endif
 
-    ikp=0
-    minbandgap=0d0
-    do ik=kpmin,min(kpmax,nkp3)
-       ikp=ikp+1
-      Hk = 0d0
-      do ir=1,nr
-        !phase = kpoint(1,ik)*rvec(1,ir)+kpoint(2,ik)*rvec(2,ir)+kpoint(3,ik)*rvec(3,ir)
-        phase = dot_product(kpoint(:,ik),rvec(:,ir))
-        HK=HK+((1-a)*(triv_Hr(:,:,ir))+(a)*(top_Hr(:,:,ir)))*dcmplx(cos(phase),-sin(phase))/float(ndeg(ir))
-        !HK=HK+(top_Hr(:,:,i)*dcmplx(cos(phase),-sin(phase))/float(ndeg(i)))
-      enddo
-      call zheev('V','U',nb,HK,nb,k_ene,work,lwork,rwork,info)
-      ene(:,ikp)=k_ene(12:13)
-  	! if (abs(k_ene(13)-k_ene(12)) < minbandgap) minbandgap = abs(k_ene(13)-k_ene(12))
-  	if (abs(k_ene(13)-k_ene(12)) < 0.00035d0) print *,abs(k_ene(13)-k_ene(12)), a,kpoint(:,ik)
-    enddo
+		dk=kmax/meshres
 
-	CALL MPI_GATHER( ENE   ,ECOUNTS,MPI_DOUBLE_PRECISION,   &
-				   ENERGY,ECOUNTS,MPI_DOUBLE_PRECISION, &
-						0,MPI_COMM_WORLD,IERR)
-    deallocate(energy)
-    call MPI_FINALIZE( IERR )
+		ik=0
+		do ikx=-meshres,meshres
+		  do iky=-meshres,meshres
+			do ikz=-meshres,meshres
+			  ik=ik+1
+			  kpoint(1,ik)=ikx*dk + kmiddle(1)
+			  kpoint(2,ik)=iky*dk + kmiddle(2)
+			  kpoint(3,ik)=ikz*dk + kmiddle(3)
+			enddo
+		  enddo
+		enddo
+
+	!----- Perform fourier transform
+
+		ikp=0
+		minbandgap=0.5d0
+		do ik=1, nkp3
+			print *, ik, "/", nkp3
+			ikp=ikp+1
+			Hk = 0d0
+			do ir=1,nr
+				phase = dot_product(kpoint(:,ik),rvec(:,ir))
+				HK=HK+((1-a)*(triv_Hr(:,:,ir))+(a)*(top_Hr(:,:,ir)))*dcmplx(cos(phase),-sin(phase))/float(ndeg(ir))
+			enddo
+			call zheev('V','U',nb,HK,nb,k_ene,work,lwork,rwork,info)
+
+			bandgap = abs(k_ene(13)-k_ene(12))	
+			if (bandgap < 0.00030093d0) then
+				print *, bandgap, a, kpoint(:,ik)
+				if (bandgap < minbandgap) then
+					minbandgap = bandgap
+					minkpoint = kpoint(:,ik)	
+				endif
+			endif
+		enddo
+		print *, "test"
+		call find_minbandgap(kmax/10,kmiddle,triv_Hr,top_Hr,rvec,ndeg)
+
+
+	END SUBROUTINE find_minbandgap
+
 end Program Projected_band_structure
-
-SUBROUTINE INIT_MPI
-    USE PARAMETERS               ,             ONLY: IERR,MYID,NUMPROCS
-    IMPLICIT NONE
-    INCLUDE 'mpif.h'
-        Call MPI_INIT( IERR )
-        Call MPI_COMM_RANK( MPI_COMM_WORLD, MYID, IERR )
-        Call MPI_COMM_SIZE( MPI_COMM_WORLD, NUMPROCS , IERR )
-!        Write(*,*) ‘Process’, myid, ' of ’, NUMPROCS , ‘is alive.’
-END SUBROUTINE INIT_MPI
