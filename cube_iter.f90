9d5f8b54da4f2b4fc46c34887ae63fedd37de71f
felixiinohayes <felixhayes1@gmail.com>
2024-10-07 22:32:30 +0100

    Update cube_iter.f90

--- cube_iter.f90
+++ cube_iter.f90
@@ -3,9 +3,9 @@ module parameters
 !--------to be modified by the user
     character(len=80):: prefix="BiTeI"
     character*1:: bmat='I'
-    character*2:: which='LM'
-    real*8,parameter::ef= 4.18903772,a=1,emin=5.5,emax=6.2,eta1=0.005,eta2=0.0005,TOL=0.0001,Bx=0.00
-    integer*8,parameter::nblocks=2,matsize=(nblocks)**3,maxiter=100000,ishift=1,mode=1,eres=1000
+    character*2:: which='SM'
+    real*8,parameter::ef= 4.18903772,a=0,emin=3.6,emax=4.8,eta1=0.3,TOL=0.0001,Bx=0.0
+    integer*8,parameter::nblocks=6,matsize=(nblocks)**3,maxiter=100000,ishift=1,mode=1,eres=70
     integer nb
     INTEGER IERR,MYID,NUMPROCS
     
@@ -21,22 +21,21 @@ Program Projected_band_structure
     integer*4 i,j,k,l,nr,ie,lwork,info,ik,count,ir,ir3,ir12,nr12,r1,r2,r3,sign,il,i1,j1,i2,j2,i3,j3,xindex,yindex,rvec_data(3),index,interp_size
     integer*4 IPARAM(11),IPNTR(14),iter,IDO,LDV,LDZ,N
     integer*8 LWORKL,NEV,NCV
-    real*8 avec(3,3),bvec(3,3),pi2,x1,x2,y1,y2,epoints(eres),a_spec,factor,p_l,de,dos(nblocks)
+    real*8 avec(3,3),bvec(3,3),pi2,x1,x2,y1,y2,epoints(eres),a_spec,factor,p_l,de,dos
     real*8,allocatable:: rvec(:,:),rwork(:)
     integer*4,allocatable:: ndeg(:),vec_ind(:,:)
     complex*16,allocatable::top_Hr(:,:),triv_Hr(:,:),super_H(:,:),surface_vec(:),B_pt(:,:)
-    complex*16,allocatable::RESID(:),V(:,:),WORKD(:),WORKL(:),D(:),WORKEV(:),Z(:,:)
+    complex*16,allocatable::RESID(:),V(:,:),WORKD(:),WORKL(:),D(:),WORKEV(:),Z(:,:),extrarow(:),extracol(:)
     complex*16,dimension(:,:,:,:,:),allocatable :: interp_Hr
     complex*16 SIGMA,b_sigma(2,2)
     logical:: rvecmat
     logical,allocatable:: select(:)
 !----Date and Time
-    character(len=8) :: date_start, time_start
-    character(len=8) :: date_end, time_end
-    real*8::end_second, start_second
-!------------------------------------------------------
+    integer,dimension(8) :: values_start
+    integer,dimension(8) :: values_end
+    call date_and_time(VALUES=values_start)
+
     pi2=4.0d0*atan(1.0d0)*2.0d0
-    call date_and_time(date_start, time_start)
 
     write(top_file,'(a,a)')trim(adjustl(prefix)),"_hr_topological.dat"
     write(triv_file,'(a,a)')trim(adjustl(prefix)),"_hr_trivial.dat"
@@ -52,16 +51,12 @@ Program Projected_band_structure
     open(99,file=trim(adjustl(top_file)))
     open(97,file=trim(adjustl(triv_file)))
 
-    open(100,file='test.dx')
-    open(200,file='DOS_layer_eigen.dat')
-    open(300,file='ene_surface.dat')
+    open(100,file='DOS_cube.dx')
+    open(150, file='cube_eigenvalues.dat')
 
 !------read H(R)
     interp_size=6
     if(abs(nblocks) > interp_size) interp_size = abs(nblocks)
-
-    !---- Magnetic Perturbation
-
 
     read(99,*)
     read(99,*)nb,nr
@@ -74,16 +69,17 @@ Program Projected_band_structure
     allocate(B_pt(nb,nb))
 
     !B along X-axis
-    B_sigma(1,:) = [dcmplx(0d0,0d0),  dcmplx(Bx,0d0)]
-    B_sigma(2,:) = [dcmplx(Bx,0d0) ,  dcmplx(0d0,0d0)]
+    ! B_sigma(1,:) = [dcmplx(0d0,0d0),  dcmplx(Bx,0d0)]
+    ! B_sigma(2,:) = [dcmplx(Bx,0d0) ,  dcmplx(0d0,0d0)]
 
     !B along Y axis
 	! B_sigma(1,:) = [dcmplx(0d0,0d0),  dcmplx(0d0,-Bx)]
     ! B_sigma(2,:) = [dcmplx(0d0,Bx) ,  dcmplx(0d0,0d0)]
 
     !B along Z-axis
-    ! B_sigma(1,:) = [dcmplx(Bx,0d0),  dcmplx(0d0,0d0)]
-    ! B_sigma(2,:) = [dcmplx(0d0,0d0) ,  dcmplx(-Bx,0d0)]
+    B_sigma(1,:) = [dcmplx(Bx,0d0),  dcmplx(0d0,0d0)]
+    B_sigma(2,:) = [dcmplx(0d0,0d0) ,  dcmplx(-Bx,0d0)]
+
 	B_pt=0d0
 	do i=1,nb
 		do j=1,nb
@@ -100,6 +96,7 @@ Program Projected_band_structure
 			endif
 		enddo
 	enddo
+
     do ir=1,nr
         do i=1,nb
             do j=1,nb
@@ -119,18 +116,17 @@ Program Projected_band_structure
 
 !-----Energy Mesh
     de = (emax-emin)/eres
-
     do i=1, eres
         epoints(i) = emin + de*i
     enddo
-
 !------ARPACK
  
-    N=nb*matsize
+    N=nb*matsize+1
     NEV=N-2
     NCV=NEV+2
     allocate(RESID(N),V(N,NCV),WORKD(N*3),WORKL(3*NCV*NCV + 5*NCV+10),RWORK(NCV))
     allocate(select(NCV),D(NEV),Z(N,NEV),WORKEV(2*NCV))
+    allocate(extracol(N),extrarow(N))
 
     iparam(1)=ishift
     iparam(3)=maxiter
@@ -146,6 +142,26 @@ Program Projected_band_structure
     rvecmat=.true.
     select=.true.
 
+!----- Lone pair passivation
+    extrarow = dcmplx(0.0d0, 0.0d0)
+    extracol = dcmplx(0.0d0, 0.0d0)
+
+    ! do k = 0, nblocks-1
+    !     do j = 0, nblocks-1
+    !         do i = 0, nblocks-1
+    !             ! Check if the point is on the x-y edges (i.e., at the boundary of the x-y plane)
+    !             if (i == 0 .or. i == nblocks-1 .or. j == 0 .or. j == nblocks-1) then
+    !                 index = i + j*(nblocks) + k*(nblocks)*(nblocks)
+    !                 extrarow(index*nb+1:(index+1)*nb+1) = dcmplx(3d-1, 0.0d0)
+    !                 extracol(index*nb+1:(index+1)*nb+1) = dcmplx(3d-1, 0.0d0)
+    !             endif
+    !         enddo
+    !     enddo
+    ! enddo
+    ! do i=1,N
+    !     if(mod(i,N/matsize)==0) print*, extrarow(i)
+    ! enddo
+
     do while (iter<maxiter)
         iter=iter+1
         print *, iter
@@ -155,7 +171,7 @@ Program Projected_band_structure
         
         if(IDO==-1 .or. IDO==1) then
             !WORKD(IPNTR(2):IPNTR(2)+N-1) = matmul(super_H,WORKD(IPNTR(1):IPNTR(1)+N-1))
-            call matmul_chunk(interp_Hr, WORKD(IPNTR(1):IPNTR(1)+N-1), WORKD(IPNTR(2):IPNTR(2)+N-1),N)
+            call matmul_chunk(interp_Hr, WORKD(IPNTR(1):IPNTR(1)+N-1), WORKD(IPNTR(2):IPNTR(2)+N-1),extrarow,extracol,N)
             ! call matmul_(interp_Hr, WORKD(IPNTR(1):IPNTR(1)+N-1), WORKD(IPNTR(2):IPNTR(2)+N-1),N,nblocks)
             ! print *, "input: ", WORKD(IPNTR(1)+2), "output", WORKD(IPNTR(2)+2)
             continue
@@ -192,58 +208,13 @@ Program Projected_band_structure
 
     allocate(surface_vec(4*nb*(nblocks-1)),vec_ind(matsize,3))
 
-!------Computes total DOS for each Z layer
-    do j=1,matsize
-    !------ Indices for blocks in eigenvector
-        r3 = ((j-1)/nblocks**2)
-        r2 = mod((j-1)/nblocks,3)
-        r1 = mod((j-1),3)
-
-        vec_ind(j,1) = r1
-        vec_ind(j,2) = r2
-        vec_ind(j,3) = r3
+    do i=1,N
+        write(150, '(f12.8)') real(d(i))
     enddo
-    print *, real(d)
 
-    ! count = 0 
-    ! do ie=1,eres
-    !     count = count + 1
-    !     print*, ie, eres
-
-    !     write(100, '(a,i8,a,i8,a,i10,a)') 'object',2+count,' class array type float rank 1 shape',1,&
-    !                             ' item', matsize, ' data follows'
-    !     !----Spectral DOS
-    !     do j=0,matsize-1
-    !         a_spec = 0d0
-    !         do i=1,N
-    !             p_l = dot_product( v( 1+(j*nb) : (j+1)*nb, i), v( 1+(j*nb) : (j+1)*nb, i))
-
-    !             factor = ((epoints(ie)- d(i)))/eta1
-    !             ! if(ie==1)print *, epoints(ie)-d(i)
-    !             ! if(ie==1)print*, p_l* (exp(-0.5d0*factor**2)) * 1/sqrt(2*pi2*eta**2)
-
-    !             a_spec = a_spec + p_l* (exp(-0.5d0*factor**2)) * 1/sqrt(2*pi2*eta1**2)
-    !         enddo
-    !         write(100, '(3(1x,f15.10))') a_spec
-    !     enddo
-    !     write(100, '(a)') 'attribute "dep" string "positions"' 
-
-    !     do j=0,nblocks-1
-    !         dos(j+1) = 0d0
-    !         do i=1,N
-    !             p_l = dot_product(v( 1+(j*(nblocks**2)*nb) : (j+1)*(nblocks**2)*nb, i), v( 1+(j*(nblocks**2)*nb) : (j+1)*(nblocks**2)*nb, i))
-    !             factor = ((epoints(ie)- d(i)))/eta2
-    !             ! if(ie==1)print *, epoints(ie)-d(i)
-    !             ! if(ie==1)print*, p_l* (exp(-0.5d0*factor**2)) * 1/sqrt(2*pi2*eta**2)
-
-    !             dos(j+1) = dos(j+1) + p_l* (exp(-0.5d0*factor**2)) * 1/sqrt(2*pi2*eta2**2)
-    !         enddo
-    !     enddo
-    !     write(200, '(7(1x,f15.10))') real(epoints(ie)),dos
-    ! enddo
-
-    count=0
-    do i=1,NEV
+    print *, "Calculating DOS..."
+    count = 0 
+    do ie=1,eres
         count = count + 1
         print*, ie, eres
 
@@ -251,81 +222,49 @@ Program Projected_band_structure
                                 ' item', matsize, ' data follows'
         !----Spectral DOS
         do j=0,matsize-1
-            p_l = dot_product( v( 1+(j*nb) : (j+1)*nb, i), v( 1+(j*nb) : (j+1)*nb, i))
-            write(100, '(3(1x,f15.10))') p_l
+            a_spec = 0d0
+            do i=1,N
+                p_l = dot_product( v( 1+(j*nb) : (j+1)*nb, i), v( 1+(j*nb) : (j+1)*nb, i))
+                factor = ((epoints(ie)- d(i)))/eta1
+                a_spec = a_spec + p_l * (exp(-0.5d0*factor**2)) * 1/sqrt(2*pi2*eta1**2)
+            enddo
+            write(100, '(3(1x,f12.10))') a_spec
         enddo
         write(100, '(a)') 'attribute "dep" string "positions"' 
-
-        do j=0,nblocks-1
-            p_l = dot_product(v( 1+(j*(nblocks**2)*nb) : (j+1)*(nblocks**2)*nb, i), v( 1+(j*(nblocks**2)*nb) : (j+1)*(nblocks**2)*nb, i))
-        enddo
-        write(200, '(7(1x,f15.10))') real(d(i)),p_l
     enddo
 
-!------Computes surface DOS for each Z layer
+    ! Check if eigenvectors are normalized
     ! do i=1,N
-    !     dos(i,:)=0d0
-    !     do j=0,nblocks-1
-    !         surface_vec = 0d0 
-    !         index=1
-    !         surface_vec(1:nb*nblocks) = v(1+ j*nb*(nblocks**2):nb*nblocks + j*nb*(nblocks**2),i)
-    !         index=index+nb*nblocks
-    !         ik=0
-    !         do k=0,nblocks-3
-    !             surface_vec(nb*nblocks+1+ik*nb:nb*(nblocks+1)+1+ik*nb) = v(index+ j*nb*(nblocks**2):index+nb-1+ j*nb*(nblocks**2),i)
-    !             ik=ik+1
-    !             index=index+(nb*(nblocks-1))
-    !             surface_vec(nb*nblocks+1+ik*nb:nb*(nblocks+1)+1+ik*nb) = v(index+ j*nb*(nblocks**2):index+nb-1+ j*nb*(nblocks**2),i)
-    !             ik=ik+1
-    !             index=index+nb
-    !         enddo
-    !         surface_vec(nb*nblocks+1+ik*nb:nb*(2*nblocks)+1+ik*nb) = v(index+ j*nb*(nblocks**2):index+nb*(nblocks)-1+ j*nb*(nblocks**2),i)
-
-    !         dos(i,j+1) = dot_product(surface_vec,surface_vec)
-    !         write(200, '(3(1x,f12.6))') real(d(i)), real(j), real(dos(i,j+1))
-
-    !     enddo 
+    !     print *, dot_product(v(:,i),v(:,i))
     ! enddo
 
-    ! do i=0,eres-1
-    !     write(100,'(A,i8,A,/,A,/,A,/,A,i8,/)') &
-    !     'object',eres+3+i,' class field', &
-    !     'component "positions" value 1', &
-    !     'component "connections" value 2', &
-    !     'component "data" value ',3+i
-    ! enddo
-    ! write(100, '(a)') 'object "series" class series'
-    ! do i=0,eres-1
-    !     write(100, '(a,i8,a,i8,a,i8)') 'member', i, ' value', (i+eres+3), ' position', i
-    ! enddo
-
-    do i=0,NEV-1
+    do i=0,eres-1
         write(100,'(A,i8,A,/,A,/,A,/,A,i8,/)') &
-        'object',NEV+3+i,' class field', &
+        'object',eres+3+i,' class field', &
         'component "positions" value 1', &
         'component "connections" value 2', &
         'component "data" value ',3+i
     enddo
     write(100, '(a)') 'object "series" class series'
-    do i=0,NEV-1
-        write(100, '(a,i8,a,i8,a,i8)') 'member', i, ' value', (i+NEV+3), ' position', i
+    do i=0,eres-1
+        write(100, '(a,i8,a,i8,a,i8)') 'member', i, ' value', (i+eres+3), ' position', i
     enddo
 
     write(100, '(A)') 'end'
 
-    ! call date_and_time(date_end, time_end)
-    ! read(time_end  , '(f10.1)') end_second
-    ! read(time_start, '(f10.1)') start_second
-    ! ! print*,"Start Time: ", trim(time_start)
-    ! print*, 'Duration: ', end_second- start_second
+    call date_and_time(VALUES=values_end)
+
+    print *, 'Start time: ', values_start(5), ':', values_start(6), ':', values_start(7)
+    print *, 'End time: ', values_end(5), ':', values_end(6), ':', values_end(7)
 
     contains
-
-        subroutine matmul_chunk(interp_Hr,vec_in,vec_out,N)  
+        subroutine matmul_chunk(interp_Hr,vec_in,vec_out,extrarow,extracol,N)  
             integer*4,intent(in)::N
             complex*16,dimension(:,:,:,:,:),allocatable :: interp_Hr
             complex*16,intent(in):: vec_in(N*3)
             complex*16,intent(out)::vec_out(N*3)
+            complex*16,intent(in)::extrarow(N)
+            complex*16,intent(in)::extracol(N)
             complex*16::tempvec(N)
 
             tempvec=0d0
@@ -350,6 +289,11 @@ Program Projected_band_structure
                         enddo
                     enddo
                 enddo
+            enddo
+
+            tempvec(N)=dot_product(extrarow, vec_in)
+            do i = 1, N-1
+                tempvec(i) = tempvec(i) + extracol(i) * vec_in(N-1)  ! Use the extra column to adjust existing elements
             enddo
             vec_out=tempvec
 
